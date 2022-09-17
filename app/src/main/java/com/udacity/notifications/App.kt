ad7a62356d56a37dbf638374f6dc4940bcbcea4b
diaa <diaaahmedh200@gmail.com>
2022-09-17 18:33:29 +0200

    Initial commit

---
+++ app/src/main/java/com/udacity/notifications/App.kt
@@ -0,0 +1,46 @@
+package com.udacity.notifications
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+
+
+class App: Application()
+{
+
+    companion object{
+         val CHANNEL_ID = "CHANNEL1"
+         val CHANNEL_ID2 = "CHANNEL2"
+    }
+    override fun onCreate() {
+        super.onCreate()
+
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel()
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            var channel1 = NotificationChannel(
+                CHANNEL_ID,"Channel Name1",NotificationManager.IMPORTANCE_HIGH
+            )
+
+            channel1.description = "This is channel1"
+
+            var channel2 = NotificationChannel(
+                CHANNEL_ID2,"Channel Name2",NotificationManager.IMPORTANCE_HIGH
+            )
+
+            channel2.description = "This is channel2"
+
+            var manager:NotificationManager = getSystemService(NotificationManager::class.java)
+
+            manager.createNotificationChannel(channel1)
+            manager.createNotificationChannel(channel2)
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/udacity/notifications/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.udacity.notifications
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.udacity.notifications.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private val ui by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    lateinit var compat:NotificationManagerCompat
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(ui.root)
+
+        compat = NotificationManagerCompat.from(this)
+
+        ui.sendChannel1.setOnClickListener{
+            sendChannel1()
+        }
+
+        ui.sendChannel2.setOnClickListener{
+            sendChannel2()
+        }
+    }
+
+    private fun sendChannel1()
+    {
+
+        var intent = Intent(this,MainActivity::class.java)
+
+        var pending_intent = PendingIntent.getActivity(this,
+        0,intent,PendingIntent.FLAG_IMMUTABLE)
+
+        var largeIcon = BitmapFactory.decodeResource(resources,R.drawable.test)
+
+
+        var notification = NotificationCompat.Builder(this,App.CHANNEL_ID)
+            .setSmallIcon(R.drawable.alert)
+            .setContentTitle("Channel 1 test")
+            .setContentText("Message")
+            .setLargeIcon(largeIcon)
+            .setColor(Color.MAGENTA)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_ALARM)
+            .setContentIntent(pending_intent)
+            .build()
+
+        compat.notify(1,notification)
+    }
+
+    private fun sendChannel2()
+    {
+        var notification = NotificationCompat.Builder(this,App.CHANNEL_ID2)
+            .setSmallIcon(R.drawable.baseline)
+            .setContentTitle("Channel 2 test")
+            .setContentText("Message channel2")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_ALARM)
+            .build()
+
+        compat.notify(2,notification)
+    }
+}
